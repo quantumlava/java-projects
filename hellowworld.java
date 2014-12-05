07c4269f144f70aea1a87cd21a1a7bff251b70bb
quantumlava <t.imothylaskoski@gmail.com>
2014-12-05 10:08:38 -0500

    save

--- hellowworld.java
+++ hellowworld.java
@@ -1,1 +1,6 @@
-System.out.println("Hello, World!");
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+}
